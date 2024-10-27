c43f56d8b30ca561c92f23d2488a34ac11a14324
Samuel Brifo <lunarhive@gmail.com>
2024-10-27 14:41:49 +0000

    an application

---
+++ src/main/java/com/codefilms/application/controllers/DemoController.java
@@ -0,0 +1,31 @@
+package com.codefilms.application.controllers;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+	
+	
+	//let's have a public method that says hello to users
+	@GetMapping(value="/greetings",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> sayHello() {
+		
+		return new ResponseEntity<String>(new JSONObject().put("greetings", "Hello").toString(), HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

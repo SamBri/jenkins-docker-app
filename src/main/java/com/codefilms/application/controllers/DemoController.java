b59dbe5b30e78fb8523e71f2ae63dc7d159e160a
Samuel Brifo <lunarhive@gmail.com>
2024-10-29 20:13:40 +0000

    cicd

--- src/main/java/com/codefilms/application/controllers/DemoController.java
+++ src/main/java/com/codefilms/application/controllers/DemoController.java
@@ -22,7 +22,7 @@ public class DemoController {
 		
 		return new ResponseEntity<String>(
 				new JSONObject()
-				.put("message", "we are in continuous deployment mode")
+				.put("message", "we are in continuous deployment mode :)")
 				.put("status", "success")
 				.put("timestamp", LocalDateTime.now())
 				.put("requestId", UUID.randomUUID().toString())

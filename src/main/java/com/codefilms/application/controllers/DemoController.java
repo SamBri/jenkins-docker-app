df19e990f97708806072a9c1e5e704565b12e59e
Samuel Brifo <lunarhive@gmail.com>
2025-04-19 20:37:45 +0000

    make docker builds dynamic name resolution

--- src/main/java/com/codefilms/application/controllers/DemoController.java
+++ src/main/java/com/codefilms/application/controllers/DemoController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,43 +20,32 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api")
 @Slf4j
 public class DemoController {
+
 	
-	
-	//let's have a public method that says hello to users
-	@GetMapping(value="/greetings",produces=MediaType.APPLICATION_JSON_VALUE)
+	@Value("${apiTextMessage}")
+	private   String apiTextMessage;
+
+	// let's have a public method that says hello to users
+	@GetMapping(value = "/greetings", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> sayHello() {
-		
-		
+
 		log.info("api endpoint called from " + System.getProperty("os.name"));
 		
-		return new ResponseEntity<String>(
-				new JSONObject()
-				.put("message", "we are in continuous deployment mode :)")
-				.put("status", "success")
-				.put("timestamp", LocalDateTime.now())
-				.put("requestId", UUID.randomUUID().toString())
-				.toString(), 
+		//apiTextMessage = System.g
+
+		return new ResponseEntity<String>(new JSONObject().put("message", apiTextMessage).put("status", "success")
+				.put("timestamp", LocalDateTime.now()).put("requestId", UUID.randomUUID().toString()).toString(),
 				HttpStatus.OK);
-	
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("java.version"));
-	
-	
+
 		System.out.println(System.getProperty("java.vendor"));
 
-		
 		System.out.println(System.getProperty("java.home"));
 
-	
 	}
-
-	
-	
-	
-	
-	
 
 }
